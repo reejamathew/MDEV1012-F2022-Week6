05298c7b73dcb94641ed436183e72464573b9ec3
Reeja Mathew <200547300@student.georgianc.on.ca>
2022-10-19 04:31:07 -0400

    adding header

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 
 import './App.css';
 
-//style and fonts
-import'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './App.css';
-
 //common components
 import Header from './components/header';
+import Footer from './components/footer';
 
 //styles and fonts
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -19,9 +15,13 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+
+      {/*Main Content */}
       <div className="container">
         <h1>Hello,World!</h1>
       </div>
+
+      <Footer></Footer>
     </div>
    
   );
---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Footer(){
+    return(
+    <nav className="navbar fixed-bottom bg-light">
+        <div className="container-fluid">
+          <h4>All Rights Reserved.Portfolio Site. &copy;Copy right 2022</h4>
+        </div>
+      </nav>
+    );
+}
+export default Footer;
